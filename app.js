73504838fa8ce9e9ce6a48d7bcb2c13278862ddf
kenzyyang <827506072@qq.com>
2019-05-15 12:55:35 +0800

    前端嵌合到后端

--- app.js
+++ app.js
@@ -6,19 +6,6 @@ const app = new Koa();
 
 const koaStatic = require('koa-static');
 
-// 访问静态资源时,为其添加content-rang
-// app.use(async (ctx, next) => {
-//     const url = ctx.request.url;
-//     await next();
-//     console.log(url);
-//     const reg = /\/audio\/([0-9]+).mp3/;
-//     if (reg.test(url)) {
-//         const id = reg.exec(url)[1];
-//         console.log('正则匹配');
-//         console.log(id);
-//     }
-// });
-
 // 静态资源目录，用于存放图片等相关信息
 const staticPath = './public';
 app.use(koaStatic(path.join(__dirname, staticPath), {
@@ -27,36 +14,6 @@ app.use(koaStatic(path.join(__dirname, staticPath), {
         response.set('Accept-Ranges','bytes');
     }
 }));
-
-// // 使用mime模块请求静态资源
-// const mime = require('mime');
-// app.use(async (ctx, next) => {
-//     const url = ctx.request.url;
-// // 1、获取用户请求的路径 req.url
-//     // 2、获取public目录的完整路径
-//     var publicDir = path.join(__dirname, 'public');
-//     // 3、根据public路径 和 用户请求的路径 计算出用户请求的静态资源的完整路径
-//     var filename = path.join(publicDir, url);
-//     console.log(filename);
-//     // 4、根据文件的完整路径去读取该文件，如果读取到了，就把文件中数据返回给用户，如果读取不到则返回404
-//     try {
-//         const file = fs.createReadStream(filename);
-//         console.log('文件读取');
-//         console.log(file);
-//         console.log(mime.getType(filename));
-//         console.log(ctx.response.header);
-//         ctx.response.headers['Content-Type'] = mime.getType(filename);
-//         ctx.response.body = file;
-//
-//     } catch (err) {
-//         console.log(err);
-//         ctx.response.body = {
-//             code: -1,
-//             message: '请求资源不存在'
-//         }
-//     }
-//     next();
-// });
 
 // 文件上传的body解析
 const koaBody = require('koa-body'); //解析上传文件的插件
--- router/index.js
+++ router/index.js
@@ -3,12 +3,20 @@
  *   @date:  2019-4-4
  *   @desc:  koa router总入口文件，通过该入口做路由分发
  * */
+const fs = require('fs');
+const path = require('path');
 const Router = require('koa-router');
 const {user} = require('./user');
 const {audio} = require('./audio');
 const {audioChapter} = require('./audioChapter');
 
 let router = new Router();
+
+// 获取页面的路由
+router.get('/', async (ctx, next) => {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream(path.join(__dirname, './demos/template.html'));
+});
 
 router.use('/user', user.routes());
 router.use('/audio', audio.routes());
